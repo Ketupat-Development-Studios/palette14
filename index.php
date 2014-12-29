5cb3ccb9fb738de0b614863e0fcabf7edb4ce53a
Huey <leejinhuey@gmail.com>
2014-12-29 11:47:23 +0800

    fully working

--- index.php
+++ index.php
@@ -3,57 +3,47 @@
 		<title>Lodestar 2014</title>
 		<link href="css/video-js.min.css" rel="stylesheet">
 		<style>
+			body{margin:0;padding:0;}
 			#main{
-				width:80%;
+				width:100%;
+				height:100%;
 				position:fixed;
-				left:10%;
-				top:10%;
+				margin-top:20px;
 			}
 			#lodestar_video{
-				width:100%;
-				margin-bottom:20px;
-			}
-			.progressBar{
-				margin-bottom:9px;
-				position: relative;
-				width: 100%;
-				height: 30px;
-				background-color: #000;
-			}
-			#timeBar{
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 0;
-				height: 100%;
-				background-color: #ccc;
-			}
-			#play-pause,.progressBar{
-				display:inline-block;
+				
 			}
 			.vjs-control-bar{
-				
+				position:relative;
+				top:100%;
+			}
+			div.video-js{
+				margin:auto;
+			}
+			.whiteDiv{
+				height:100%;
+				position:fixed;
+				background-color:#fff;
+				z-index:1000;
+			}
+			#leftBlock{
+				left:0;
+			}
+			#rightBlock{
+				right:0;
 			}
 		</style>
 	</head>
 
 	<body>
+		<div class="whiteDiv" id="leftBlock"></div>
 		<div id="main">
 			<video id="lodestar_video" poster="img/hitchhike.jpg" preload="auto"
-				class="video-js vjs-default-skin" controls>
+				class="video-js vjs-default-skin" controls width="960px" height="540px">
 	 			<source src="http://comsci.club/earlydraft.mp4" type="video/mp4">
 			</video>
-			<!--<div id="controls">
-				<div id="play-pause"><img src="img/ic_play_circle_outline_48px.svg"></div><--
-			 -<div class="progressBar">
-					<div id="timeBar"></div>
-				</div>
-				<!--<div class="progressBar">
-				   <div id="bufferBar"></div>
-				</div>--
-			</div>
-		</div>-->
-
+		</div>
+		<div class="whiteDiv" id="rightBlock"></div>
 		<script src="js/jquery-2.1.3.min.js"></script>
 		<script src="js/video.js"></script>
 		<script>
@@ -64,70 +54,30 @@
 				 myPlayer.play();
 			});
 
-			/*
-			var playIcon = '<img src="img/ic_play_circle_outline_48px.svg">';
-			var pauseIcon = '<img src="img/ic_pause_circle_outline_48px.svg">';
-			var controlsWidth;
-
-			var jQueryVideo = $('#lodestar_video');
-			var video = jQueryVideo[0];
+			var jQueryVideo = $('#lodestar_video_html5_api'); //jQuery object
+			var video = jQueryVideo[0]; //DOM object
 
 			$(document).ready(function(){
+				jQueryVideo.width("1920");
+				$(".whiteDiv").css('width',($(window).width()-960)/2 + 'px');
 				panToLeft();
-			});
-
-			$('#play-pause').on('click', function() {
-			   if(video.paused) {
-			   		$(this).html(pauseIcon);
-			    	video.play();
-			   } else {
-			   		$(this).html(playIcon);
-			    	video.pause();
-			   }
-			   return false;
-			});
-
-			$(window).on('resize load',function(){
-				controlsWidth = $("#controls").width();
-				$('.progressBar').width(controlsWidth-48);
-			});
-
-			//get HTML5 video time duration
-			jQueryVideo.on('loadedmetadata', function() {
-			   $('.duration').text(video.duration);
-			});
- 
-			//update HTML5 video current play time
-			jQueryVideo.on('timeupdate', function() {
-			   var currentPos = video.currentTime; //Get currenttime
-			   var maxduration = video.duration; //Get video duration
-			   var percentage = currentPos / maxduration; //in %
-			   $('#timeBar').css('width', ((controlsWidth - 48)*percentage)+'px');
 			});
 
 			$(document).keypress(function(e) {
 				console.log('audio switch');
-			    if(e.which == 82) {
+			    if(e.which == 114) {
 			        if(gainL.gain.value == 1){
+			        	jQueryVideo.css('margin-left','-960px');
 			        	panToRight();
+			        	console.log('right');
 			        } else {
+			        	jQueryVideo.css('margin-left','0');
 			        	panToLeft();
+			        	console.log('left');
 			        }
 			    }
 			});
-			/*
-			video.onprogress = function(){
-			    var maxduration = video.duration;
-				var currentBuffer = video.buffered.end(0);
-				var percentage = 100 * currentBuffer / maxduration;
-				console.log(percentage);
-				$('#bufferBar').css('width', percentage+'%');
-
-				if(currentBuffer < maxduration) {
-					setTimeout(startBuffer, 500);
-				}
-			}*/
 		</script>
-		<!--<script src="js/audioPanner.js"></script>-->
+		<script src="js/audioPanner.js"></script>
 	</body>
 </html>
